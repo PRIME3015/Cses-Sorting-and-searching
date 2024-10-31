58fecb16b5f38d09e5a5016470d3e6e2c04b5ead
PRIME3015 <133253302+PRIME3015@users.noreply.github.com>
2024-10-31 15:57:12 +0530

    first

---
+++ Nested_Ranges_Count.cpp
@@ -0,0 +1,240 @@
+// राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण
+// राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+// राधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+//  राधाकृष्णराधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+//  राधाकृष्णराधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+// राधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+//  राधाकृष्णराधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्ण राधाकृष्णराधाकृष्ण राधाकृष्णराधाकृष्णराधाकृष्ण राधाकृष्ण
+// ॐ सह नाववतुॐ सह नाववतु मंत्र । ॐ सहना ववतु | सह नौ भुनक्तु  सह वीर्यम करवावाहै तेजस्वी अवधीतमस्तु मां विदविशावाहै ॐ ॐ सह नाववतु । सह नौ भुनक्तु । सह वीर्य करवाहै । तेजस्वि नावधीतमस्तु मा विद्विषावहै । ॐ शान्तिः शान्तिः शान्तिः ॥
+//  श्रीकृष्णाय वासुदेवाय हरये परमात्मने । प्रणतः क्लेशनाशाय गोविंदाय नमो नमः ॥
+#include <math.h>
+#include <algorithm>
+#include <climits>
+#include <iomanip>
+#include <iostream>
+#include <cstring>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+#include <numeric>
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+
+/* clang-format off */
+ 
+ 
+#define ll long long int 
+#define pii pair<int, int>
+#define pll pair<long long int , long long int >
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<long long int >
+#define mii map<int, int>
+#define mll map<ll,ll>
+#define si set<int>
+#define sll set<long long int>
+#define sc set<char>
+#define f first
+#define s second
+#define endl "\n"
+#define sp <<" "
+#define pb push_back
+#define MOD 1000000007
+#define fora(a) for(auto u:a)
+//#define gcd(a,b) __gcd(a,b)
+//#define lcm(a,b) (a*(b/gcd(a,b)))
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define test int tc; cin>>tc; while(tc--)
+#define forn(i,n) for(auto i=0; i<n; i++)
+#define printv(a) {for(auto u:a) cout<<u<<" "; cout<<endl;}
+#define printm(a) {for(auto u:a) cout<<u.f sp u.s<<endl;}
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fori(a,b,c) {for(a = c.begin(); a!=b; a++) cout<< *a<< " "; cout<<endl;}
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define ip(v) for(auto &x:v) cin>>x;
+#define debug(v) for(auto &x:v) cout<<x<<" ";
+//typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vl;
+//typedef vector<int> vi;
+//typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<int>::iterator vit;
+//typedef set<int> si;
+//typedef map<int, int> mii;
+const double eps = 1e-9;
+/* FUNCTIONS */
+#define ff(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+ 
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+ 
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+//ll min(ll a,ll b) { if (a<b) return a; return b; }
+ll min(ll a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,ll b) { if (a>b) return a; return b; }
+//ll max(ll a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+ 
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+/* clang-format on */
+
+/* Main()  function */
+
+// Header files, namespaces,
+// macros as defined above
+
+//  typedef tree<int, null_type, less<int>, rb_tree_tag,
+// tree_order_statistics_node_update >
+//     ordered_set;
+
+
+
+vector<vector<ll>> seg;
+void build(ll s, ll e, ll idx, vll &vp)
+{
+    if (s == e)
+    {
+        seg[idx].pb(vp[s]);
+        return;
+    }
+    ll mid = (s + e) / 2;
+    build(s, mid, 2 * idx + 1, vp);
+    build(mid + 1, e, 2 * idx + 2, vp);
+   
+    merge(all(seg[2 * idx + 1]), all(seg[2 * idx + 2]), back_inserter(seg[idx]));
+}
+
+// Query to count elements <= val in range [l, r]
+ll query(ll s, ll e, ll l, ll r, ll idx, ll val)
+{
+    if (s > r || e < l)
+        return 0; // Out of range
+    if (s >= l && e <= r)
+    {
+        
+        return upper_bound(all(seg[idx]), val) - seg[idx].begin();
+    }
+    ll mid = (s + e) / 2;
+    ll left = query(s, mid, l, r, 2 * idx + 1, val);
+    ll right = query(mid + 1, e, l, r, 2 * idx + 2, val);
+    return left + right;
+}
+
+ll query1(ll s, ll e, ll l, ll r, ll idx, ll val)
+{
+    if (l > e || r < s)
+        return 0;
+    if (s >= l && e <= r)
+    {
+
+        return seg[idx].end() - lower_bound(all(seg[idx]), val);
+    }
+    ll mid = (s + e) >> 1;
+    ll left = query1(s, mid, l, r, 2 * idx + 1, val);
+    ll right = query1(mid + 1, e, l, r, 2 * idx + 2, val);
+    return left + right;
+}
+
+static bool cmp(vll &v1, vll &v2)
+{
+    if (v1[0] == v2[0])
+        return v1[1] > v2[1];
+    return v1[0] < v2[0];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+    seg = vector<vector<ll>>(4 * n + 100);
+
+    vector<vector<ll>> vp(n, vector<ll>(3, 0));
+    ff(i, 0, n)
+    {
+        vp[i][2] = i;
+        cin >> vp[i][0] >> vp[i][1];
+    }
+
+    sort(all(vp), cmp);
+
+    vll v, temp, indexr;
+    for (auto &x : vp)
+    {
+        v.pb(x[0]);
+        temp.pb(x[1]);
+        indexr.pb(x[2]);
+    }
+
+    build(0, n - 1, 0, temp);
+
+    vll ans1(n, 0);
+    vll ans2(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        ll idx = upper_bound(v.begin(), v.end(), temp[i]) - v.begin();
+        idx--;
+        ll ans = 0;
+        if (i + 1 < n && idx >= 0 && idx < n)
+            ans = query(0, n - 1, i + 1, idx, 0, temp[i]);
+        // cout<<i<<" "<<ans<<endl;
+        if (ans > 0)
+            ans1[indexr[i]] = ans;
+
+        ll anc = 0;
+        if (i - 1 >= 0)
+        {
+            anc = query1(0, n - 1, 0, i - 1, 0, temp[i]);
+            if (anc > 0)
+                ans2[indexr[i]] = anc;
+        }
+    }
+
+    for (auto &x : ans1)
+    {
+        cout << x << " ";
+    }
+
+    cout << endl;
+    for (auto &x : ans2)
+    {
+        cout << x << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
